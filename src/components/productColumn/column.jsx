2654507846c3820a8e7fc9ba6f8458c5c3961682
okikiola94 <farominiyiopeyemi@gmail.com>
2022-11-16 15:53:28 -0800

    feat: update mobile views

--- src/components/productColumn/column.jsx
+++ src/components/productColumn/column.jsx
@@ -1,38 +1,32 @@
 import "./column.css";
-
-
-const Column =()=>{
-    return(
+ 
+ 
+const Column = () => {
+    return (
         <div className="Product-column_wrapper">
- <div className="inner-column_wrapper" id="inner_column-wrapper">
-        <div className="Mb_token-Wrapper">
-        <div className="Mb_token-image_wrapper" id="Mb_token-img_wrapper">
-        <img src={`${process.env.PUBLIC_URL}/assets/images/mbtoken_icon.png`}  id ="mbtoken_icon"  alt="mbtoken_icon"/>
-        </div> 
-        <div className="mb-token-text_wrapper" id="mb-token_text-wrapper">
-            <div className="mbtoken-text">MBToken</div>
-        </div>
-        </div>
-
-        <div className="MetaMask-Wrapper">
-        <div className="Metamask-image_wrapper" id="Metamask-img_wrapper">
-        <img src={`${process.env.PUBLIC_URL}/assets/images/metamask_icon.png`}  id ="Metamask_icon"  alt="Metamask_icon"/>
-        </div> 
-        <div className="Metamask-text_wrapper" id="Metamask_text-wrapper">
-            <div className="metamask-text">METAMASK</div>
-        </div>
-        </div>
-
-        <div className="opensea-Wrapper">
-        <div className="opensea-image_wrapper" id="opensea-img_wrapper">
-        <img src={`${process.env.PUBLIC_URL}/assets/images/opensea_icon.png`}  id ="opensea_icon"  alt="opensea_icon"/>
-        </div> 
-        <div className="opensea-text_wrapper" id="opensea_text-wrapper">
-            <div className="opensea-text">opensea</div>
-        </div>
-        </div>
-    </div>
+ 
+            <span className="column_child">
+                <img src={`${process.env.PUBLIC_URL}/assets/images/mbtoken_icon.png`} className='column_image' alt="mbtoken_icon" />
+                <p> MBToken</p>
+ 
+            </span>
+            <span className="column_child">
+                <img src={`${process.env.PUBLIC_URL}/assets/images/metamask_icon.png`} className='column_image' alt="Metamask_icon" />
+                <p> Metamask</p>
+            </span>
+            <span className="column_child">
+                <img src={`${process.env.PUBLIC_URL}/assets/images/opensea_icon.png`} className='column_image' alt="opensea_icon" />
+                <p>Opensea</p>
+            </span>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
         </div>
     );
 }
+
 export default Column;
