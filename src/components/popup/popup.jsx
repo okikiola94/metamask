6961586e4abe3f1df5a9de816137d06e981424f8
okikiola94 <farominiyiopeyemi@gmail.com>
2022-11-16 19:32:44 -0800

    popup update

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -1,60 +1,35 @@
 import "./popup.css";
 import React from "react";
-import Wallet from "../wallet/wallet";
+import Walletpopup from '../walletpopup/walletpopup';
 import { Link } from "react-router-dom";
-import { useState } from "react";
-
-
-
-
+ 
+ 
+ 
+ 
 const PopUp = (props) => {
-    const [isNavExpanded, setIsNavExpanded] = useState(false);
-    return ( 
-      <div className="popup-backdrop"  >
-       
-        <div className="popup-body" onClick={(props.exit)}>
-        <img  onClick={(props.exit)} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADKSURBVHgB7ZQ9DsMgDIUdugLqCNyiN+iN2huUbh17nW7N1qOww9CNGilIHQh/yZZ8EoojXuzEfgFgZ5tIKS9KqVuLHtc1tUdmnhm897qmyKR5ov4ILQghNL6VzxUJe0ETtNBDrsji5LkiLckPJYFz7k0pHTDUnHNgjJ3DfHDdjTEa1iJ+SWtbSLWQEJ+KSxRbFAg9j23B2xGmdllrR1hKaqA1Fu5OvlqRGit2F2nx+Z+7kmdRcsjo+xNeXujzBxSI/wnyxfgDO9vjB11ZYo38vKDAAAAAAElFTkSuQmCC" alt="close" id="close-popup"/>
-
+  return (
+    <div className="popup-backdrop"  >
+ 
+        <img onClick={(props.exit)} className='popup_icon' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADKSURBVHgB7ZQ9DsMgDIUdugLqCNyiN+iN2huUbh17nW7N1qOww9CNGilIHQh/yZZ8EoojXuzEfgFgZ5tIKS9KqVuLHtc1tUdmnhm897qmyKR5ov4ILQghNL6VzxUJe0ETtNBDrsji5LkiLckPJYFz7k0pHTDUnHNgjJ3DfHDdjTEa1iJ+SWtbSLWQEJ+KSxRbFAg9j23B2xGmdllrR1hKaqA1Fu5OvlqRGit2F2nx+Z+7kmdRcsjo+xNeXujzBxSI/wnyxfgDO9vjB11ZYo38vKDAAAAAAElFTkSuQmCC" alt="close" id="close-popup" />
+ 
         <div
-        className="popup-navigation-menu" id="nav-pop">
-        <ul className="navigation-popup_">
-          <li>
-          <div className="link-container-popup">
+          className="popup-navigation-menu" id="nav-pop">
           <Link to="/" id="contact">Home</Link>
-          </div>
-          </li>
-          <li>
-          <div className="link-container">
+         
           <Link to="/places" id="contact">Place to stay</Link>
-          </div>
-          </li>
-          <li>
-          <div className="link-container">
-          <Link to="/" id="contact">NFTs</Link>
-          </div>
-          </li>
-          <li>
-          <div className="link-container">
-          <Link to="/" id="contact">Community</Link>
-          </div>
-          </li>
-
-<li onClick={() => {
-      setIsNavExpanded(!isNavExpanded);}}className="connect-wallet">
-    connect wallet     
-</li>
-{isNavExpanded && <Wallet exit={() => setIsNavExpanded(false)} />
-  }
-        </ul>
-        
-
-
-        </div>
-        
+ 
+ 
+          <Link to="/fullname" id="contact">NFTs</Link>
+ 
+          <Link to="/fullname" id="contact">Community</Link>
+          <Walletpopup />
+ 
+ 
       </div>
-      </div>
-  
-    )
-  }
-  
-  export default PopUp;
+    </div>
+ 
+  )
+}
+ 
+export default PopUp;
+
